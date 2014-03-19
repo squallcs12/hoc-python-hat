4049ccb791a0da4c0f0551249701b6c4030867ef
Hat Dao <enjoy3013@gmail.com>
2014-03-20 04:16:07 +0700

    Write first view script

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^polls/', include('polls.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
