e48265acc5580442430287db611a60a56fba9e08
Hat Dao <enjoy3013@gmail.com>
2014-03-20 03:28:48 +0700

    Add custom method for Question model

--- polls/models/choice_model.py
+++ polls/models/choice_model.py
@@ -3,7 +3,10 @@ from django.db import models
 class Choice(models.Model):
     question = models.ForeignKey("polls.Question")
     choice_text = models.CharField(max_length=200)
-    votes = models.IntergerField(default=0)
+    votes = models.IntegerField(default=0)
+    
+    def __unicode__(self):
+        return self.choice_text
     
     class Meta:
         app_label = "polls"
